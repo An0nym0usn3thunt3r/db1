289dd3e4193917eb02dd192f5f5d256b57564911
HeliParmar <hellyparmar17@gmail.com>
2024-04-29 20:38:08 +0530

    Merge changes from remote main branch

--- dashB/src/App.jsx
+++ dashB/src/App.jsx
@@ -1,8 +1,19 @@
 import { AreaChart } from '@tremor/react';
 
 export default function App() {
-  return (
+  return ( 
+    <div>
       < AreaChart/>
-  
+      <div  className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow">
+        <div className='flax'>
+          <img src="icon1.jpg" alt="" />
+          <h5 className="mb-2 font-normal tracking-tight text-gray-700">Number of companies</h5>
+        </div>
+        <div className='flex '>
+          <p className=" text-2xl font-bold text-gray-900 ">2</p>
+          {/* chart code */}
+        </div>
+      </div>
+    </div>
   )
 }
